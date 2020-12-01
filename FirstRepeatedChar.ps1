3a793caeb85807bd800abb34e969a7891d5ba956
Donald Schulz <donald@thewebweweave.com>
2020-12-01 08:25:00 -0800

    Initial Commit

---
+++ FirstRepeatedChar.ps1
@@ -0,0 +1,27 @@
+﻿function Find-FirstRepeatedChar 
+{
+    Param ([string] $str = 'abcdedcba')
+    
+    $h = @{};    
+
+    $charArray = $str.ToCharArray();
+
+    for ($i=0; $i -lt $str.Length; $i++) 
+    {
+        $c = $charArray[$i];
+        if ($h.Contains($c))
+        {
+            return $c;
+        }
+
+        $h.Add($c, 0);
+    }
+
+    return "No Repeat Characters Found"
+}
+
+
+#$str = 'abcdedcba'
+#$char = $str.ToCharArray();
+#$char[3];
+#$char[7];
